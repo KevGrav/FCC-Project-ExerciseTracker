39dfc955e7679bf1e8ff57aee0e4db61b3ed852c
Kev Grav <kgravell.prsvr@gmail.com>
2024-03-05 12:43:58 -0500

    more updates

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.DB_URL);
 
 const UserSchema = new Schema({
   username: String,
