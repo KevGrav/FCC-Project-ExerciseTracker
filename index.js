67dbc9e23dbe6757c2a803c2e79c66577d6004fd
Kev Grav <kgravell.prsvr@gmail.com>
2024-03-06 13:25:07 -0500

    final

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-require('dotenv').config();
+const express = require('express')
+const app = express()
+const cors = require('cors')
+require('dotenv').config()
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-mongoose.connect(process.env.DB_URL);
+mongoose.connect(process.env.DB_URL)
 
 const UserSchema = new Schema({
-  username: String,
+  username: String
 });
 const User = mongoose.model('User', UserSchema);
 
@@ -20,9 +20,9 @@ const ExerciseSchema = new Schema({
 });
 const Exercise = mongoose.model('Exercise', ExerciseSchema);
 
-app.use(cors());
-app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(cors())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
@@ -62,18 +62,18 @@ app.post('/api/users/:_id/exercises', async (req, res) => {
       res.send("Could not find user")
     } else {
       const exerciseObj = new Exercise({
-        user_id: user._id,
         description,
         duration,
         date: date ? new Date(date) : new Date(),
+        user_id: user._id
       })
       const exercise = await exerciseObj.save()
       res.json({
-        _id: user._id,
         username: user.username,
         description: exercise.description,
         duration: exercise.duration,
-        date: new Date(exercise.date).toDateString(),        
+        date: new Date(exercise.date).toDateString(),
+        _id: user._id        
       })
     }
   } catch(err) {
